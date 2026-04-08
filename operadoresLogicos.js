f47f6dca469af448677b34b0828bbe863c546439
ricap-p <ricardo.prudencio@outlook.com>
2026-04-08 16:30:40 -0300

    Training for e css

---
+++ operadoresLogicos.js
@@ -0,0 +1,19 @@
+let div = document.getElementById("result");
+let input = document.getElementById("searchInput");
+
+const contacts = [
+    { name: "Alice", phone: "123-456-7890" },
+    { name: "Bob", phone: "987-654-3210" },
+    { name: "Charlie", phone: "555-555-5555" }
+];
+
+
+searchButton.addEventListener("click", function() {
+    for (let i = 0; i < contacts.length; i++) {
+        let contact = contacts[i];
+
+        if (input.value === contact.name) {
+            div.innerHTML = `Name: ${contact.name}, Phone: ${contact.phone}`;
+        }
+    }
+});
